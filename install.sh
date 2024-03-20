bb1aa2d3a847302293c92f8477bc5b0df5233938
owenkojola <okojola@outlook.com>
2024-03-20 09:10:45 -0400

    Update install.sh

---
+++ install.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run this script as root"
+    exit 1
+fi
+
+if ! command -v apt >/dev/null 2>&1; then
+    echo "This script requires apt package manager, which is not available on your system."
+    exit 1
+fi
+
+apt update
+apt install -y dnsutils nmap
+
+ANACONDA_URL="https://repo.anaconda.com/archive/Anaconda3-2024.02-1-Linux-x86_64.sh"
+TEMP_DIR=$(mktemp -d)
+cd "$TEMP_DIR" || exit
+curl -O "$ANACONDA_URL"
+bash "Anaconda3-latest-Linux-x86_64.sh" -b -p /opt/anaconda3
+
+USER=$(ls -ld /home/* | awk '{print $3}' | head -n 1)
+
+chown -R "$USER":"$USER" /opt/anaconda3
+
+rm -rf "$TEMP_DIR"
+
+apt install -y bsdgames
+
+apt install -y awscli
+
+echo "Installation completed successfully."
+
+#!/bin/bash
+
+cat <<EOT >> ~/.bashrc
+alias cl="clear"
+alias ll="ls -al"
+
+export EDITOR=nano
+export VISUAL=nano
+alias pico='edit'
+alias spico='sedit'
+alias nano='edit'
+alias snano='sedit'
+EOT
+
+cp ~/.bashrc ~/dotfiles/.bashrc
+
+ln -sf ~/dotfiles/.bashrc ~/.bashrc
+
+echo "Aliases added to .bashrc, copied to git repository, and symbolic link created successfully."
+
+if [ ! -d ~/.ssh ]; then
+    mkdir -p ~/.ssh
+fi
+
+cp ~/.ssh/authorized_keys ~/dotfiles/authorized_keys
+ln -sf ~/dotfiles/authorized_keys ~/.ssh/authorized_keys
+
+cat <<EOT > ~/.ssh/config
+    Host fry.cs.wright.edu
+    HostName fry.cs.wright.edu
+    User W001owk
+EOT
+ln -sf ~/.ssh/config ~/dotfiles//config
+
+echo "Configuration completed successfully."
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+mkdir -p ~/.vim/colors
+curl -o ~/.vim/colors/gruvbox.vim https://raw.githubusercontent.com/morhetz/gruvbox/master/colors/gruvbox.vim
+
+git clone https://github.com/preservim/nerdtree.git ~/.vim/bundle/nerdtree
+
+cat <<EOT >> ~/.vimrc
+set nocompatible
+filetype off
+
+set rtp+=~/.vim/bundle/Vundle.vim
+call vundle#begin()
+
+" Plugin to manage plugins
+Plugin 'VundleVim/Vundle.vim'
+
+" Color scheme
+colorscheme gruvbox
+
+" Vim improvement plugin
+Plugin 'preservim/nerdtree'
+
+call vundle#end()
+filetype plugin indent on
+EOT
+
+echo "Vundle installed"
